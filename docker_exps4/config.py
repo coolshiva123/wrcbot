eace3bdd6df8d88314831d1c4193c0d567b773f1
Shivaramakrishnan G <shivaramakrishnan.g>
2025-06-23 19:24:08 +0530

    new docker image created after local testing

---
+++ docker_exps4/config.py
@@ -0,0 +1,13 @@
+BACKEND = 'SlackV3'
+BOT_DATA_DIR = './data'
+BOT_EXTRA_PLUGIN_DIR = './plugins'
+BOT_LOG_FILE = './errbot.log'
+BOT_LOG_LEVEL = 'INFO'
+BOT_ADMINS = ('@awsterraform30',)
+BOT_IDENTITY = {
+    'token': '',   # Replace with your bot's token
+    'signing_secret': '',  # Replace with your bot's signing secret
+    'app_token': ''
+     }
+BOT_PREFIX = '<>'  # Replace with your bot's user ID
+BOT_EXTRA_BACKEND_DIR = './backends'
